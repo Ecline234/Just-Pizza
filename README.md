eed6608644aeb4d7dcc091b02aae49176d69e41b
Ecline Koech <eclinejerutokoech@gmail.com>
2021-06-30 07:02:01 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Just-Pizza
+An application for my customer to order pizza.
+
+#### By:
+
+* Ecline Koech
+
+## Description
+
+ A web application where customers order different sizes of pizzas with one or more toppings.
+
+## Setup/Installation Requirements
+
+* Install visual studio code
+* Git clone repository
+* Each contibutor create a branch
+* Ensure to have live server extension installed
+* Click on Go Live
+
+## Known Bugs
+
+No bugs.
+
+### Technologies Used 
+
+* Html
+* Css
+* Bootstrap
+* Jquery
+* Javascript
+
+## Support and contact details
+
+Email: eclinejerutokoech@gmail.com
+
+### License
+
+[MIT License](./LICENSE)
+
+Copyright (c) 2021
--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-$(document).ready(function(){
-    $("#table").hide();
-    $("#order").click(function(){
-        var pizzaType = $("")
-    })
-})
+const pizzaType = ["H"]
+
+
+// $(document).ready(function(){
+//     $("#table").hide();
+//     $("#order").click(function(){
+//         var pizzaType = $("#pizzatype option:selected").val();
+//         var sizeOfPizza = $("#size option:selected").val();
+//         var toppingsOfpizza = $("#toppings option:selected").val();
+//         var crustOfpizza = $("#crust option:selected").val();
+//         var total = parseInt(typeOfPizza) + parseInt(toppingsOfPizza) + parseInt(sizeOfPizza) + parseInt(crustOfpizza)
+//         var order = 1;
+//         var grandTotal = 0;
+        
+//         $(table).show();
+//         $(order).hide();
+//     });
+// });
